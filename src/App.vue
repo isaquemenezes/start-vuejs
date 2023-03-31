3166c99f9b5eddd21333d855fb635380f6456572
Isaque Silva <isaque.silva@equilibriumweb.com>
2023-03-31 09:29:19 -0300

    uplaod

--- src/App.vue
+++ src/App.vue
@@ -2,71 +2,63 @@
   
 
   <div>
-    <h1>Propriedade Computada </h1>
+    <h1>Observadores Watch </h1>
     <p>v-model</p>
+    
+    <input 
+      v-model="name"
+      type="text"
+    /> <br>
+    {{ name }}
    
   </div>
-   <p>Fist Name: {{ user.fisrt_name }}</p>
-   <p>Last Name: {{ user.last_name }}</p>
-    <button @click="click()">Click</button>
+  <br> <br>
+  
   <div>
+    <h2>Páginação de sistema</h2>
     
+    <select v-model="pageCount" name="" id="">
+      <option value="5">5</option>
+      <option value="10">10</option>
+      <option value="15">15</option>
+      <option value="20">20</option>
+    </select> <br>
+
+    {{ pageCount }}
   </div>
   <br><br><br>
 
   <div>
 
-    <p>Nome Completo</p>
-    {{ fullName }}
-    <br><br><br>
-    <div
-      v-for="todo in todos"
-      :key="todo.id"
-    >
-    {{ todo.title  }}
+    <label for="">First name:</label>
+    <input
+      v-model="user.first_name"
+      type="text" 
+    /> <br><br>
+    <p> {{ user.first_name }}</p>
    
-    
+
+    <label for="">Last name:</label>
+    <input
+      v-model="user.last_name"
+      type="text" 
+    /> <br> <br>
+    {{ user.last_name }}   
+
+  </div>
+
+
+  <br><br><br>
+
+    <div>
+      
 
     </div>
-
-  </div>
-  <br><br><br>
-
-  <div>
-    <p>Não completos </p>
-    <p 
-      v-for="todo in uncompletedTodos" 
-      :key="todo.id"
-    >
-      {{  todo.completed }}</p>
-    <br><br>
-    
-    <p>completos </p>
-    <p
-      v-for="todo in completedTodos"
-      :key="todo.id"
-    >
-      {{  todo.completed }}
-    </p>
-
-  </div>
   <br><br><br>
 
   <div>
     
-    <p>Todos</p>
-    <p
-      v-for="todo in todos"
-      :key="todo.id"   
-    >
-      <input 
-        v-model="todo.completed"
-        type="checkbox"
-      >
-
-    {{ todo.title }} / {{ todo.completed }}
-    </p>
-
+   
   </div>
   <br><br><br>
 
@@ -74,10 +66,6 @@
   <div>
     
   </div>
-
-  
-
-  
   
 </template>
 
@@ -91,76 +79,61 @@ export default {
 
   data() {
     return {
+      name: '',
+      pageCount:5,
       user: {
-        fisrt_name: 'Joao',
-        last_name: 'Marcos'
-      },
-
-      todos: 
-        // 20230330093311
-        // https://jsonplaceholder.typicode.com/todos
-            [
-              {
-                "userId": 1,
-                "id": 1,
-                "title": "delectus aut autem",
-                "completed": false
-              },
-              {
-                "userId": 1,
-                "id": 2,
-                "title": "quis ut nam facilis et officia qui",
-                "completed": false
-              },
-              {
-                "userId": 1,
-                "id": 3,
-                "title": "fugiat veniam minus",
-                "completed": false
-              },
-              {
-                "userId": 1,
-                "id": 4,
-                "title": "et porro tempora",
-                "completed": true
-              },
-              {
-                "userId": 1,
-                "id": 5,
-                "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-                "completed": false
-              },
-              {
-                "userId": 1,
-                "id": 6,
-                "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-                "completed": false
-              }
-        
-            ]
+        first_name: '',
+        last_name: ''
+      }
      
-    
     }
+  },
+
+  watch: {
+    // name(new_value, old_value) {
+    //   console.log(new_value, old_value);
+    // }
+    
+    // name() {
+    //   this.saveName();
+    // }
+
+    name(vl) {
+      if (vl.length >= 3)
+      {
+        this.saveName();
+      }
+    },
+
+    pageCount() {
+      this.changePage();
+    },
+
+    user: {
+      handler() {
+        console.log("Usr alterado!");
+      },
+      deep:true
+    }
+   
+
   },
 
   methods: {
-    click() {
-      console.log(this.user);
-    }
+   saveName() {
+    console.log('Ajax');
+    console.log(this.name);
+   },
+
+   changePage() {
+    console.log('Ajax');
+    console.log('Change Page');
+
+   }
   },
 
   computed: {
-    fullName() {
-     return `${this.user.fisrt_name} ${this.user.last_name}` 
-    },
-
-    uncompletedTodos() {
-      return this.todos.filter(todo => !todo.completed);      
-    },
-
-    completedTodos() {
-      return this.todos.filter(todo => todo.completed);      
-    },
+  
   },
 
   
