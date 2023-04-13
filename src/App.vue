95d053a88de8c77e0374d057aa46256b64477945
Isaque Silva <isaque.silva@equilibriumweb.com>
2023-04-13 16:19:38 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,35 @@
 <template>
-
-
-  <div>
-    <h4>admin</h4>
-    <h3>First Name</h3>
-    {{ obj_user.first_name }}
-
-    <h3>Last Name</h3>
-    {{ obj_user.last_name }}
-  </div>
+  <AppHook
+    v-if="showAppHook" 
+  />
 
   <button
-    @click="obj_user.last_name = 'Obito' "
+  @click="showAppHook = !showAppHook"
   >
-  update    
+toggle
   </button>
-
-  
-
 
 </template>
 
 <script>
-import { ref, computed, watch} from 'vue'
+import { ref} from 'vue'
+import AppHook from '@/components/AppHook.vue'
 
 export default {
-  name: 'App',
-  
+  components :{
+    AppHook
+  },
 
 
   setup() {
+
+    const showAppHook = ref(true)
    
-    const obj_user = ref({
-      
-      first_name: 'hokage',
-      last_name: 'folha'
-    
-    })
-
-    // const fullName = computed(() => {
-    //   return `${obj_user.value.first_name} ${obj_user.value.last_name}`
-    // })
-
-    // refactory
-    const fullName = computed(() => 
-      `${obj_user.value.first_name} ${obj_user.value.last_name}`
-    )
-
-    // watch(obj_user, () => {
-    //   console.log('observou !');
-    // }, {
-    //   deep:true
-    // });
-
-    // call back 
-    watch(() => obj_user.value.first_name, () => {
-      console.log('observou !');
-    });
 
     return {
+
+      showAppHook
   
-      obj_user,
-      fullName
-      
        
     }
   }
